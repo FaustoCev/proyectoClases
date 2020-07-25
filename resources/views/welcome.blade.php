7d35110cbca796a173ed9850686899f23e68d5ec
Fausto Cevallos <faustodcevallosm@gmail.com>
2020-07-24 19:53:54 -0500

    h1 - migration posts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Iniciail
                 </div>
 
                 <div class="links">
